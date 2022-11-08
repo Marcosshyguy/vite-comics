87a35851a6e90a7297dd44f9736300270e6a5965
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-11-08 17:52:36 +0100

    create the header

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script >
+import AppHeader from './components/AppHeader.vue';
 export default{
   data(){
     return {
@@ -9,16 +10,18 @@ export default{
 
   },
   components:{
-
+    AppHeader
   }
 }
 </script>
 
 <template>
-  <h1>dsfsg</h1>
+  
+  <AppHeader/>
+  
 </template>
 
 <style lang="scss">
-@use "./styles/general.scss";
+@use "./styles/general.scss" as *;
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,91 @@
+<script >
+export default{
+    data(){
+        return {
+            logoImage : 'dc-logo.png',
+            itemListHeader : [{
+                    headerLink : '/characters',
+                    linkName : 'characters',
+                    linkState : true
+                },
+                {
+                    headerLink : '/comics',
+                    linkName : 'comics',
+                    linkState : false
+                },
+                {
+                    headerLink : '/movies',
+                    linkName : 'movies',
+                    linkState : false
+                },
+                {
+                    headerLink : '/tv',
+                    linkName : 'tv',
+                    linkState : false
+                }   
+            ]
+        }
+    },
+    methods :{
+        getImagePath(imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        }
+    },
+    components:{
+
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="header-container">
+            <div>
+                <img :src="getImagePath(`../assets/img/${logoImage}`)" alt="">
+            </div>
+            <div class="header-list">
+                <ul>
+                    <li v-for="(link, linkIndex) in this.itemListHeader" :key="linkIndex"><a href="">{{link.linkName}}</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/general.scss" as *;
+@use "../styles/paertials/variables.scss" as *;
+@use "../styles/paertials/mixins.scss" as *; 
+
+    header{
+        height: $header-height;
+        
+
+            .header-container{
+                @include page-centering();
+                height: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+
+                div:first-child{
+                    height: 90%;
+
+                    img{
+                        height: 100%;
+                        width: 100%;
+                        object-fit: cover;
+                    }
+                }
+
+                div:last-child{
+
+                    ul{
+                        display: flex;
+                        gap: 1em;
+                    }
+                }
+            }
+    }
+
+</style>
