0b75ae34c257ec0cf056945730501655e948f3f4
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-11-08 16:20:36 +0100

    create more file taht determinate the different section of the page that web importo in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script >
+export default{
+  data(){
+    return {
 
+    }
+  },
+  methods :{
+
+  },
+  components:{
+
+  }
+}
 </script>
 
 <template>
-  <h1>kdkdk</h1>
+  <h1>dsfsg</h1>
 </template>
 
 <style lang="scss">
-
-h1{
-  color: red;
-}
+@use "./styles/general.scss";
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 </style>
